18c18713b760226fd5f6d8954db202b133474962
Alekhya-220452 <n220452@rguktn.ac.in>
2026-02-04 07:10:16 +0530

    string_functions and variables_scope.php are added and login and registration.php are updated

--- registration.php
+++ registration.php
@@ -13,16 +13,35 @@ $username=$_POST['username'];
 $email=$_POST['email'];
 $password=$_POST['password'];
 
-$hashed=password_hash($password,PASSWORD_DEFAULT);
+//$hashed=password_hash($password,PASSWORD_DEFAULT);
 
-$sql="insert into userinfo(username,email,password)
-    values('$username','$email','$hashed')";
+//$sql="insert into userinfo(username,email,password)
+    //values('$username','$email','$hashed')";
 
-if($conn->query($sql)===true){
-    echo "registartion successful";
-}  
-else{
-    echo "insert failed";
+//if($conn->query($sql)===true){
+    //echo "registartion successful";
+//}  
+//else{
+    //echo "insert failed";
+//}
+
+$username=trim($username);
+$email=trim($email);
+
+echo $email;
+echo "uppercase: ".strtoupper($username)."<br>";
+
+if(strlen($username)<5){
+    die("username  must be at least 5 charcters");
+
 }
+
+if(strlen($password)<6){
+    die("password  must be at least 6 charcters");
+    
+}
+
 $conn->close();
+
+
 ?>
